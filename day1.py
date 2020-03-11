353d499b59a46e906c8743e9e0ebf95adbb9b7b3
itsnicoleking <nicole.s.king@outlook.com>
2020-03-11 06:15:08 +0000

    day 1 part 2

--- day1.py
+++ day1.py
@@ -1,9 +1,26 @@
-from array import *
+import array
+from math import floor
 
 sum = 0
+# lineSum = 0
+file = open("ItsNicoleKing-aoc-19/input.txt", "r")
 
-f = open("ItsNicoleKing-aoc-19/input.txt", "r")
+# part 1
+# for line in file:
+#   sum = sum + floor(int(line) / 3) - 2
+
+# print("part 1: " + str(sum) )
+
+# part 2
 
 
-for x in f:
-  print(x) 
+def lineSum(mass):
+  fuel = (floor(mass / 3) - 2)
+  if fuel <= 0:
+    return 0
+  return fuel + lineSum(fuel)
+
+for line in file:
+  sum += lineSum(int(line))
+
+print(sum)
